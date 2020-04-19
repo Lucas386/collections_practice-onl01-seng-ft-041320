7ee2bb4c4aa670a659b50d137151b9a048d23086
Lucas Easley <lucaseasley11@gmail.com>
2020-04-19 17:28:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a,b| a[1]<=>b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|arr| arr.delete(arr[2]).insert(2,"$")}
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+def sum_array_inject(array)
+    array.inject do |sum,number|
+        sum + number
+    end
+end
+def add_s_collect(array)
+      array.each_with_index.collect do |word,index|
+        if index != 1
+          word = word + "s"
+        else
+          word = word
+        end
+  end
+end
